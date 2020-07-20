97c35b9e05172ebb9c0108e3b0e73807806df2b3
Ryanjie <ryanjiena@163.com>
2020-07-21 00:28:49 +0800

    :pencil: Updating insrall.aria2.sh

--- aria2/install.aria2.sh
+++ aria2/install.aria2.sh
@@ -13,10 +13,12 @@ mkdir -p /root/.aria2 && cd /root/.aria2
 touch aria2.session aria2.log
 
 # 下载配置文件
-wget 
-wget
-wget
-wget 
+wget https://cdn.jsdelivr.net/gh/Ryanjiena/Blog@master/aria2/aa
+wget https://cdn.jsdelivr.net/gh/Ryanjiena/Blog@master/aria2/aria2.conf
+wget https://cdn.jsdelivr.net/gh/Ryanjiena/Blog@master/aria2/clean.sh
+wget https://cdn.jsdelivr.net/gh/Ryanjiena/Blog@master/aria2/delete.sh
+wget https://cdn.jsdelivr.net/gh/Ryanjiena/Blog@master/aria2/dht.dat
+wget https://cdn.jsdelivr.net/gh/Ryanjiena/Blog@master/aria2/dht6.dat
 
 # 增加执行权限
 chmod +x clean.sh
